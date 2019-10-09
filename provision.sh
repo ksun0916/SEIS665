f1da56a08f25c580abd5c9f78c95b2aba9ade54b
EC2 Default User <ec2-user@ip-172-31-47-76.ec2.internal>
2019-10-09 23:07:00 +0000

    update assignment 3

--- provision.sh
+++ provision.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 ACTION=${1:-launch}
-if [ -z "$1" ]
-	then
-		sudo yum update -y
-		sudo amazon-linux-extras install nginx1.12 -y
-		sudo chkconfig nginx on
-		sudo aws s3 cp s3://sk1911-assignment-3/index.html /usr/share/nginx/html/index.html
-		sudo service nginx start
-	else
-		version="1.0.0"
-		function show_version() {
-			echo $version
-			}
-		function remove_file() {
-			sudo service nginx stop
-			rm -fr /usr/share/nginx/html/"${1}"
-			sudo yum remove nginx -y
-			}
-		function display_help() {
-			cat << EOF
-			Usage: ${0} {-v|--version|-r|--remove|-h|--help} <filename>
-			OPTIONS:
-				-v | --version	Display the version
-				-r | --remove	Remove a file
-				-h | --help	Display the command help
-			Examples:
-			Display the version:
-				$ ${0} -v foo.txt
-			Remove a file:
-				$ ${0} -r foo.txt
-			Display help:
-				$ ${0} -h
+version="1.0.0"
+
+function update(){
+	sudo yum update -y
+        sudo amazon-linux-extras install nginx1.12 -y
+        sudo chkconfig nginx on
+        sudo aws s3 cp s3://sk1911-assignment-3/index.html /usr/share/nginx/html/index.html
+        sudo service nginx start
+}
+
+function show_version() {
+	echo $version
+}
+
+function remove_file() {
+	sudo service nginx stop
+        sudo rm -fr /usr/share/nginx/html/"${1}"
+        sudo yum remove nginx -y
+}
+
+function display_help() {
+	cat << EOF
+        Usage: ${0} {-v|--version|-r|--remove|-h|--help} <filename>
+        OPTIONS:
+        	-v | --version  Display the version
+                -r | --remove   Remove a file
+                -h | --help     Display the command help
+                Examples:
+                Display the version:
+                                $ ${0} -v foo.txt
+                Remove a file:
+                                $ ${0} -r foo.txt
+                Display help:
+                                $ ${0} -h
 EOF
 }
 
+
+if [ -z "$1" ]
+	then
+		update
+	else
 		case "$ACTION" in
 			-h| --help)
 				display_help
@@ -50,4 +58,3 @@ EOF
 			exit 1
 		esac
 fi
-
